be96e0d7100538ecfe850b4c51d70ab88b3ed61e
Dmytro Kovbasiuk <k0vbasyuk.dim0n@gmail.com>
2024-06-16 14:26:46 +0300

    done

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,17 @@
+const regFormSubmit = document.querySelector('.login-form');
+
+regFormSubmit.addEventListener("submit", event => {
+	event.preventDefault();
+    const regFormSubmit = event.target;
+    const email = regFormSubmit.elements.email.value;
+    const password = regFormSubmit.elements.password.value;
+
+    if (email === "" || password === ""){
+
+  alert('All form fields must be filled in');
+  return;
+    }
+    console.log(`Login: ${email}, Password: ${password}`);
+    regFormSubmit.reset();
+});
 
--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,18 @@
+const bgColGenegate = document.querySelector('.widget')
+
+const changeColor = document.querySelector(".change-color");
+const outputColor = document.querySelector(".color");
+
+changeColor.addEventListener("click", () => {
+  const newColor = getRandomHexColor();
+  bgColGenegate.style.backgroundColor = newColor;
+  outputColor.textContent = newColor;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
